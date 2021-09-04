6eac89d15705f45c97104963d945be7a0c85a781
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-09-04 15:53:07 -0700

    :new: Add Function Runner.run

--- covid_xray_classsification/cmd.py
+++ covid_xray_classsification/cmd.py
@@ -5,3 +5,7 @@ class Runner:
     def __init__(self,
                  command=[]):
         self.command = command
+    def run(self):
+        result = __cmd__(command)
+        if result.returncode != 0:
+            raise Exception(f"Command execution returned non-zero exit status: {result.returncode}")
