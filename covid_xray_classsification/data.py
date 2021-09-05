f20f05f18479965e865b72757bc93729aacec0a6
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-09-04 19:09:00 -0700

    📂Add Directory for Unit Tests

--- covid_xray_classsification/data.py
+++ covid_xray_classsification/data.py
@@ -1,4 +1,5 @@
 from pandas import read_csv as __readcsv__
+from covid_xray_classsification.cmd import Runner as __runner__
 
 
 class Downloader:
@@ -9,3 +10,17 @@ class Downloader:
     def __init__(self,
                  dataset='andyczhao/covidx-cxr2'):
         self.dataset_id = dataset
+    def download(self, saveTo='dataset'):
+        """
+        Download dataset to specified location, creating the specified location should it not exist at time of function invocation.
+
+        Args:
+             saveTo(str): Path to save dataset to.
+        """
+        # Create our directory in case it does not exist yet.
+        __runner__(command=['mkdir',
+                            '-p', saveTo]) # Specify the path of the directory we wish to create.
+        # Download dataset to specified path.
+        __runner__(command=['kaggle', 'datasets', 'download',
+                            '-d', self.dataset,  # Specify dataset we wish to download.
+                            '-p', saveTo])  # Specify the directory to which we wish to download our dataset of choice.
