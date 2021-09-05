1a69612e9a097bc341a7875a4c9c8a98468225f1
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-09-05 13:13:24 -0700

    🆕Add option to unzip dataset after download
    Also clean up data.Downloader.download() and fix a few of its bugs

--- covid_xray_classsification/data.py
+++ covid_xray_classsification/data.py
@@ -14,20 +14,41 @@ class Downloader:
         self.dataset = dataset
         del dataset
 
-    def download(self, saveTo='dataset'):
+    def download(self,
+                 save_to='dataset',
+                 unzip=True):
         """
         Download dataset to specified location, creating the specified location should it not exist at time of function invocation.
 
         Args:
-             saveTo(str): Path to save dataset to.
+             save_to(str): Path to save dataset to. (default 'dataset')
+             unzip(bool): Whether or not to unzip the dataset after downloading it. (default True)
         """
+        # ---- Part 1/2: Setup ----
         # Create our directory in case it does not exist yet.
-        __runner__(command=['mkdir',
-                            '-p', saveTo]) # Specify the path of the directory we wish to create.
+        create_directory = __runner__(command=['mkdir',
+                                               '-p', save_to]) # Specify the path of the directory we wish to create.
+
+        # Form command to download dataset
+        dataset_download_command = [
+            'kaggle', 'datasets', 'download',   # Specify the base command.
+            '-d', self.dataset, # Specify dataset we wish to download.
+            '-p', save_to]
+
+        # Check if we need to unzip the dataset after download.
+        if unzip is True:
+            # Specify the --unzip flag for kaggle to unzip dataset after download.
+            dataset_download_command.append('--unzip')
+
         # Download dataset to specified path.
-        __runner__(command=['kaggle', 'datasets', 'download',
-                            '-d', self.dataset,  # Specify dataset we wish to download.
-                            '-p', saveTo])  # Specify the directory to which we wish to download our dataset of choice.
+        download_dataset = __runner__(command=dataset_download_command)  # Specify the directory to which we wish to download our dataset of choice.
+
+        # ---- Part 2/2: Runtime ----
+        # Run the command to create a directory in case we need to.
+        create_directory.run()
+
+        # Run the command to download our dataset, optionally unzipping it.
+        download_dataset.run()
 
 
 class Reshape:
