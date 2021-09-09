a7f4fe04ce9b0e22611b381969d5c782f97339ef
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-09-09 10:03:21 -0700

    ⏱Set the EarlyStopping Callback to stop training once Validation Precision maxes out

--- example.py
+++ example.py
@@ -54,7 +54,7 @@ model = net.model
 # Here we use the EarlyStopping callback to stop training if the validation accuracy stops increasing.
 # This both saves a significant amount of power and usually decreases the total number of epochs to a fraction of what most will end up specifying
 # We also make sure that this callback will automatically pick the best epoch at the end of training.
-callbacks = [EarlyStopping("val_accuracy",
+callbacks = [EarlyStopping("val_precision",
                            patience=10,
                            mode='max',
                            restore_best_weights=True)]
